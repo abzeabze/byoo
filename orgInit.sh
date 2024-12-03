61e9ca5066d333b33f79905d3fd8769e535254f3
abzeabze <abze@wearehackerone.com>
2024-12-03 17:18:48 +0000

    edits2

--- orgInit.sh
+++ orgInit.sh
@@ -1,5 +1,5 @@
 sfdx  plugins:link Schema-Exporter #
-sfdx schema:helpdemo -m "asd"
+sfdx  schema:helpdemo -m "asd"
 #sfdx shane:org:create -f config/project-scratch-def.json -d 1 -s --json --userprefix int -o work.shop
 #sfdx force:source:push
 #sfdx force:user:permset:assign -n CustomerID
