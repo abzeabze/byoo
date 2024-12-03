264bd8718911f8485b8fa8aa0e8384d3fa94412e
abzeabze <abze@wearehackerone.com>
2024-12-03 10:38:19 +0000

    edit1337

--- orgInit.sh
+++ orgInit.sh
@@ -1,5 +1,5 @@
-sfdx --json `whoami`
-sfdx shane:org:create -f config/project-scratch-def.json -d 1 -s --json --userprefix int -o work.shop whoami
+sfdx plugins:link node_modules/sfdmu
+sfdx shane:org:create -f config/project-scratch-def.json -d 1 -s --json --userprefix int -o work.shop
 sfdx force:source:push
 sfdx force:user:permset:assign -n CustomerID
 sfdx shane:user:password:set -l User -g User -p sfdx1234 --json
